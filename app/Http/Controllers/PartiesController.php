5e79e5be0637a979f287d0995ae8d5935ab4904e
isao_e <isao0214@gmail.com>
2017-12-30 16:30:07 +0900

    __constructをPartiesController.phpに定義

--- app/Http/Controllers/PartiesController.php
+++ app/Http/Controllers/PartiesController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Party;
 
 class PartiesController extends Controller
 {
-    //
+    public function __construct ()
+    {
+        $this->middleware('auth');
+        parent::__construct();
+    }
 }
