c039d5f1c5057e073af5e64f6a91b080d0a333ac
isao_e <isao0214@gmail.com>
2017-12-31 16:27:17 +0900

    views/home.blade.phpにpartyの一覧を表示する

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,33 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-                <a href="/parties/create">create</a>
-                <a href="/parties/show">show</a>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    You are logged in!
-                </div>
-            </div>
+            <a href="/parties/create"><button>パーティを新規作成</button></a>
+            <p>あなたが作成したパーティ</p>
+            <table border="1">
+                <tr>
+                    <th>No</th>
+                    <th>ID</th>
+                    <th>タイトル</th>
+                    <th>詳細</th>
+                    <th>ステータス</th>
+                    <th>開始日時</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                    @forelse ($parties as $party)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $party->id }}</td>
+                            <td>{{ $party->title }}</td>
+                            <td>{{ $party->description }}</td>
+                            <td>{{ $party->displayStatus() }}</td>
+                            <td>{{ $party->displayStartAt() }}</td>
+                            <td><a href="parties/{{ $party->random_id }}"><button>詳細</button></a></td>
+                            <td><a href="parties/{{ $party->random_id }}/edit"><button>編集</button></a></td>
+                        </tr>
+                    @empty
+                    @endforelse
+            </table>
         </div>
     </div>
 </div>
