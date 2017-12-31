583f5e27c889d0a87fab3388f604d90f5e427fd5
isao_e <isao0214@gmail.com>
2017-12-31 16:28:16 +0900

    HomeController@indexでデータを渡す

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Party;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+        $res['parties'] = Party::where('user_id', $user->id)->get();
+        return view('home', $res);
     }
 }
