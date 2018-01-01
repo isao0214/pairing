3e01b0307f61195fb2a70297d7413431d4c11789
isao_e <isao0214@gmail.com>
2018-01-01 22:39:28 +0900

    parties/showの表示データを修正

--- resources/views/parties/show.blade.php
+++ resources/views/parties/show.blade.php
@@ -27,6 +27,27 @@
                 </tr>
             </table>
             <br>
+            <table border="1">
+                <tr>
+                    <td>No</td>
+                    <td>名前</td>
+                    @forelse($party->polls as $poll)
+                        <td>第{{ $loop->iteration }}希望</td>
+                    @empty
+                    @endforelse
+                </tr>
+                @forelse($party->polls as $poll)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $poll->user->name }}</td>
+                        @forelse($poll->users as $user)
+                            <td>{{ $user->name }}</td>
+                        @empty
+                        @endforelse
+                    </tr>
+                @empty
+                @endforelse
+            </table>
             <td><a href="parties/{{ $party->random_id }}/edit"><button>編集</button></a></td>
             <td><a href="home"><button>戻る</button></a></td>
         </div>
